e31fe8c323d240267bbc5e8577875da9b49c2c20
helllth <th@cismet.de>
2017-04-06 17:40:39 +0200

    removing all files in prep folder and sending the date string as second param to the backup scripts

--- milquetoast.sh
+++ milquetoast.sh
@@ -7,26 +7,27 @@ MT_RCLONE_TARGET=gdrive:/cismet/backups/$MT_MAIN_NAME
 MT_BACKUPS=/var/backups/milquetoast/backups
 MT_BACKUP_PREPS=/var/backups/milquetoast/preparation
 MT_BACKUPD=/etc/backup.d/
+TODAY=$(date +"%Y%m%d")
 echo
 echo "-------------------------------------------------------------"
 echo "-- Milquetoast ... No backup no pitty "
 echo "-------------------------------------------------------------"
 echo
-echo '... doing the work for '$(date +"%Y%m%d")
+echo '... doing the work for '$TODAY
 echo
 #delete prep folder
-rm $MT_BACKUP_PREPS/$(date +"%Y%m%d")*
+rm $MT_BACKUP_PREPS/*
 
 #execute simple backup scripts
 cd $MT_BACKUPD
 for backupstep in $(ls -1 [0-9+]* 2> /dev/null)
     do
-        $MT_BACKUPD/$backupstep $MT_BACKUP_PREPS
+        $MT_BACKUPD/$backupstep $MT_BACKUP_PREPS $TODAY
 done
 
 #create a single backup file for today
 cd $MT_BACKUP_PREPS
-tar -zcvf $MT_BACKUPS/$(date +"%Y%m%d").$MT_MAIN_NAME.tar.gz ./$(date +"%Y%m%d")*
+tar -zcvf $MT_BACKUPS/$TODAY.$MT_MAIN_NAME.tar.gz ./$TODAY*
 
 #delete old backups
 echo "rotate-backups --daily=10 --weekly=5 --monthly=13 --yearly=5 $MT_BACKUPS"
