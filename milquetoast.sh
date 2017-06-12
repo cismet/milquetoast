153aede33bc13a69c44096df6c21b3829304179e
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2017-06-12 12:08:20 +0200

    tar -P option and minor changes

--- milquetoast.sh
+++ milquetoast.sh
@@ -2,11 +2,11 @@
  
 ## config
 set +o allexport
-#MT_MAIN_NAME=<HOSTNAME>
-#MT_RCLONE_CONF=<PATH_TO_RCLONE.CONF>
-#MT_RCLONE_TARGET=<CLOUDTYPE>:<SOME_PATH>/$MT_MAIN_NAME
-#MT_BACKUPS=<SOME_PATH>
-#MT_BACKUP_PREPS=<SOME_PATH>
+#MT_MAIN_NAME=<HOSTNAME>		
+#MT_RCLONE_CONF=<PATH_TO_RCLONE.CONF>		
+#MT_RCLONE_TARGET=<CLOUDTYPE>:<SOME_PATH>/$MT_MAIN_NAME		
+#MT_BACKUPS=<SOME_PATH>		
+#MT_BACKUP_PREPS=<SOME_PATH>		
 #MT_BACKUPD=<PATH_TO_BACKUP.D_DIR>
 . /etc/milquetoast.conf
 set -o allexport
@@ -17,32 +17,34 @@ echo "-------------------------------------------------------------"
 echo "-- Milquetoast ... No backup no pitty "
 echo "-------------------------------------------------------------"
 echo
-echo '... doing the work for '$TODAY
+echo "... doing the work for $TODAY"
 echo
 
+echo "* cleanup"
 
-#delete prep folder
 if [ ! -z "$MT_BACKUP_PREPS" ]; then
-    rm $MT_BACKUP_PREPS/*
+    rm "$MT_BACKUP_PREPS/*"
 fi
 
-
 #execute simple backup scripts
+echo "* executing backup steps:"
 cd $MT_BACKUPD
 for backupstep in $(ls -1 [0-9+]* 2> /dev/null)
     do
-        $MT_BACKUPD/$backupstep $MT_BACKUP_PREPS $TODAY
+        echo "  -> $backupstep"
+        $MT_BACKUPD/$backupstep $MT_BACKUP_PREPS $TODAY 
 done
 
 #create a single backup file for today
+echo "* creating single backup file for today"
 cd $MT_BACKUP_PREPS
-tar -zcvf $MT_BACKUPS/$TODAY.$MT_MAIN_NAME.tar.gz ./$TODAY*
+tar -zcPf $MT_BACKUPS/$TODAY.$MT_MAIN_NAME.tar.gz ./$TODAY*
 
 #delete old backups
 echo "rotate-backups --daily=10 --weekly=5 --monthly=13 --yearly=5 $MT_BACKUPS"
 
 #externalize
-echo "externalize ..."
+echo "* externalize"
 
 docker run -t --rm \
   -e PUID=$(id -u backup) \
@@ -50,6 +52,7 @@ docker run -t --rm \
   -v $MT_RCLONE_CONF:/home/.rclone.conf \
   -v $MT_BACKUPS:/data \
   farmcoolcow/rclone \
-    sync /data $MT_RCLONE_TARGET
+    sync /data gdrive:/cismet/backups/$MT_MAIN_NAME
+
 echo
 echo "------------------------------------------------------- done."
